6347f8dd331aa8d4033c6d734b39fe84eae60368
Mamie Wang <szmamie@pennmedicine.upenn.edu>
2019-02-11 16:00:10 -0500

    Saved marker genes as internal object

--- data-raw/export_regev_cell_cycle.R
+++ data-raw/export_regev_cell_cycle.R
@@ -1,3 +1,4 @@
 cell_cycle_genes <- readr::read_csv('data/regev_lab_cell_cycle_genes.txt', col_names = F)
 cell_cycle_genes <- unlist(cell_cycle_genes$X1)
-usethis::use_data(cell_cycle_genes, internal = TRUE, overwrite = T)
+marker_genes <- readr::read_csv("data/markers.csv")
+usethis::use_data(marker_genes, cell_cycle_genes, internal = TRUE, overwrite = T)
