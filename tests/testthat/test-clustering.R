75d1443d1054f76d5f80e49fdb6bcd0c61d27440
Mamie Wang <szmamie@pennmedicine.upenn.edu>
2019-03-13 13:45:30 -0400

    Skip test if not on slurm

--- tests/testthat/test-clustering.R
+++ tests/testthat/test-clustering.R
@@ -2,6 +2,9 @@ context("PCA computation")
 library(robustSingleCell)
 library(rslurm)
 
+SLURM = system('sinfo', ignore.stdout = TRUE, ignore.stderr = TRUE)
+SLURM_MSG = 'Only test on Slurm head node.'
+
 data.path <- system.file("extdata", package = "robustSingleCell")
 raw_LCMV1 <- as.matrix(read.table(file.path(data.path, "LCMV1_small.txt"), check.names = F))
 LCMV1 <- initialize.project(datasets = "LCMV1",
@@ -18,17 +21,17 @@ LCMV1 <- read.data(LCMV1,
 
 LCMV1 <- get.variable.genes(LCMV1)
 
-LCMV1 <- PCA(LCMV1, local = T)
-
 test_that("The number of 'significant' PCs ", {
+  if (SLURM) skip(SLURM_MSG)
+  LCMV1 <- PCA(LCMV1, local = T)
   expect_equal(dim(LCMV1$PCA), c(7, 97))
 })
 
 context("Clustering computation")
 
-LCMV1 <- cluster.analysis(LCMV1, knn.ratios = c(0.1, 0.2), loadPreviousKnn = F,
-                          rerun = T, deleteCache = T, plot = F, local = T)
-
 test_that("The number of clusters ", {
+  if (SLURM) skip(SLURM_MSG)
+  LCMV1 <- cluster.analysis(LCMV1, knn.ratios = c(0.1, 0.2), loadPreviousKnn = F,
+                            rerun = T, deleteCache = T, plot = F, local = T)
   expect_equal(LCMV1$clustering$nclusters, 6)
 })
